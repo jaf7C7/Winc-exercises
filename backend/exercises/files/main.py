749051a589d6a9b3c84e4e0bb3833bd2ef078701
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-05-29 22:14:43 +0000

    Add 'files' exercise

--- backend/exercises/files/main.py
+++ backend/exercises/files/main.py
@@ -4,24 +4,48 @@ __human_name__ = "files"
 
 # Add your code after this line
 
-
 import os
+import zipfile
 
 
 def clean_cache():
-    cache_path = "cache"
-    # Create dir 'cache' in cwd if it doesn't exist already
-    if not os.path.isdir(cache_path):
+    cache_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "cache"
+    )
+    if os.path.isdir(cache_path):
+        for root, dirs, files in os.walk(cache_path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+    else:
         os.mkdir(cache_path)
-    # If the cache is not empty recursively delete all files and subdirs, but
-    # leave the cache dir itself
-    #
-    # TODO: Check if this deletes all files in all subdirs recursively
-    #
-    for dir_name, sub_dirs, files in os.walk("cache"):
-        for file in files:
-            os.remove(os.path.join(dir_name, file))
-        if dir_name != cache_path:
-            os.rmdir(dir_name)
 
-clean_cache()
+
+def cache_zip(zip_file, dir):
+    # Unpack 'zip_file' into 'dir'
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+        zip_ref.extractall(dir)
+
+
+def cached_files():
+    cache_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "cache"
+    )
+    cached_files = []
+    for root, dirs, files in os.walk(cache_path):
+        for file in files:
+            cached_files.append(os.path.join(root, file))
+    return cached_files
+
+
+def find_password(files):
+    for file in files:
+        with open(file, "r") as file:
+            for line in file:
+                if "password" in line:
+                    return line[len("password: "):-1]  # '-1' to omit '\n'
+
+
+if __name__ == "__main__":
+    print(find_password(cached_files()))
