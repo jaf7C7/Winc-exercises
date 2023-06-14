36b09f96366a4402575716492c96c9431728246a
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-06-14 13:30:18 +0100

    Add usage info

--- backend/superpy/super.py
+++ backend/superpy/super.py
@@ -2,6 +2,17 @@
 __winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
 __human_name__ = "superpy"
 
+
+# USAGE:
+#
+# superpy report [ inventory | revenue | profit | date ] [ --today | --yesterday | --date <date> ]
+# superpy buy --product <name> --price <price> --quantity <quantity> --expires <date>
+# superpy sell --product <name> --price <price> --quantity <quantity>
+# superpy --set-date <date>
+# superpy --advance-date <days>
+# superpy --list-expired [<date>]
+
+
 import argparse
 import csv
 from datetime import date
